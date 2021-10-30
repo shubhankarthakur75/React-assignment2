6040f6c183c979f0fd9ad9d049cbca65a12136d0
Shubhankar Thakur <shubhankarthakur75@.com>
2021-10-30 11:35:44 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
-
         <h1>Employee Feedback Form</h1>
         <div className="form-container">
           <Form />
